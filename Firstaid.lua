66f17aca3fe98b2b8908495a43a1a409168cdaf4
Jeremy Regnerus <jeremy_regnerus@msn.com>
2025-04-30 11:47:48 -0400

    Initial Commit of Recipe Databases

---
+++ Firstaid.lua
@@ -0,0 +1,169 @@
+local _, rm = ...
+
+rm.recipeDB[129] = {
+	[1251] = {
+		difficulty = {
+			1,
+			30,
+			45,
+			60,
+		},
+		item = 1251,
+		skill = 1,
+		spell = 3275,
+	},
+	[2581] = {
+		difficulty = {
+			40,
+			50,
+			75,
+			100,
+		},
+		item = 2581,
+		skill = 40,
+		spell = 3276,
+	},
+	[3530] = {
+		difficulty = {
+			80,
+			80,
+			115,
+			150,
+		},
+		item = 3530,
+		skill = 80,
+		spell = 3277,
+	},
+	[3531] = {
+		difficulty = {
+			115,
+			115,
+			150,
+			185,
+		},
+		item = 3531,
+		skill = 115,
+		spell = 3278,
+	},
+	[6450] = {
+		difficulty = {
+			150,
+			150,
+			180,
+			210,
+		},
+		item = 6450,
+		skill = 150,
+		spell = 7928,
+	},
+	[6452] = {
+		difficulty = {
+			80,
+			80,
+			115,
+			150,
+		},
+		item = 6452,
+		skill = 80,
+		spell = 7934,
+	},
+	[6454] = {
+		difficulty = {
+			130,
+			130,
+			165,
+			200,
+		},
+		item = 6453,
+		skill = 130,
+		spell = 7935,
+	},
+	[8545] = {
+		difficulty = {
+			240,
+			240,
+			270,
+			300,
+		},
+		item = 8545,
+		skill = 240,
+		spell = 10841,
+	},
+	[14529] = {
+		difficulty = {
+			260,
+			260,
+			290,
+			320,
+		},
+		item = 14529,
+		skill = 260,
+		spell = 18629,
+	},
+	[14530] = {
+		difficulty = {
+			290,
+			290,
+			320,
+			350,
+		},
+		item = 14530,
+		skill = 290,
+		spell = 18630,
+	},
+	[16084] = {
+		item = "Expert",
+		skill = 125,
+	},
+	[16085] = {
+		item = "Artisan",
+		skill = 200,
+	},
+	[16112] = {
+		difficulty = {
+			180,
+			180,
+			210,
+			240,
+		},
+		item = 6451,
+		skill = 180,
+		spell = 7929,
+	},
+	[16113] = {
+		difficulty = {
+			210,
+			210,
+			240,
+			270,
+		},
+		item = 8544,
+		skill = 210,
+		spell = 10840,
+	},
+	[19442] = {
+		difficulty = {
+			300,
+			300,
+			330,
+			360,
+		},
+		item = 19440,
+		repFaction = 529,
+		repLevel = 6,
+		skill = 300,
+		spell = 23787,
+	},
+	[232434] = {
+		difficulty = {
+			0,
+			315,
+			327,
+			340,
+		},
+		item = 232433,
+		season = "SoD",
+		skill = 300,
+		spell = 470349,
+	},
+}
---
+++ Fishing.lua
@@ -0,0 +1,12 @@
+local _, rm = ...
+
+rm.recipeDB[356] = {
+	[16082] = {
+		item = "Artisan",
+		skill = 200,
+	},
+	[16083] = {
+		item = "Expert",
+		skill = 125,
+	},
+}
